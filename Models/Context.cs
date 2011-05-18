0779cca1ede5bec37ed9f64f51f23a5e42d5de5f
Hongfei Xiao <13554344846@163.com>
2011-05-18 22:36:18 +0800

    update

--- Models/Context.cs
+++ Models/Context.cs
@@ -11,7 +11,7 @@ namespace TSS.Models
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.ComplexType<Specialty>;
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
