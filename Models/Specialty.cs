986125c07c5ea3b6efb0ac0195916dbdb70e8f68
Hongfei Xiao <13554344846@163.com>
2011-05-18 14:49:55 +0800

    Update

---
+++ Models/Specialty.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TSS.Models
+{
+    [Table("SPECIALTY")]
+    public class Specialty
+    {
+        [Key,DatabaseGenerated(System.ComponentModel.DataAnnotations.DatabaseGeneratedOption.None)]
+        public string Code { get; set; }
+
+        [MaxLength(250)]
+        public string Name { get; set; }
+    }
+}
