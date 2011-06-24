9083e15ed2f87678c6325ae71f5fff2ba7c4cf67
HuGuo <guohu2003@gmail.com>
2011-06-24 08:45:48 +0800

    u

--- Models/Context.cs
+++ Models/Context.cs
@@ -32,6 +32,10 @@ namespace TSS.Models
         public IDbSet<Role> Roles { get; set; }
         public IDbSet<Right> Rights { get; set; }
         public IDbSet<ExpRecord> ExpRecords { get; set; }
+        public IDbSet<ComprehensiveReport> ComprehensiveReports { get; set; }
+        public IDbSet<SpecialtyAnalysis> SpecialtyAnalysises { get; set; }
+        public IDbSet<IndicatorAnalysis> IndicatorAnalysises { get; set; }
+        public IDbSet<Indicator> Indicators { get; set; }
         public Context()
             : base("TSS") {
             Database.SetInitializer<Context>(new DatabaseInitializer());
@@ -47,6 +51,8 @@ namespace TSS.Models
             modelBuilder.Entity<ExpTemplate>().Property(p => p.HTML).HasColumnType("text");
             modelBuilder.Entity<Experiment>().Property(p => p.HTML).HasColumnType("text");
             modelBuilder.Entity<Right>().HasKey(p => p.Id).Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<ExpAttachment>().HasKey(p => p.Id).Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
         }
     }
 
@@ -55,49 +61,6 @@ namespace TSS.Models
         protected override void Seed(Context context) {
             base.Seed(context);
 
-            var systemManagementModules = new List<Module> {
-                new Module { Name = "��Ա����", Url = "Employee" },
-                new Module { Name = "�豸����", Url = "Equipment" },
-                new Module { Name = "ʵ�����", Url = "Experiment" },
-                new Module { Name = "����������", Url = "Workflow" },
-                new Module { Name = "ģ�����", Url = "Module" }
-            };
-
-            var specialtyModules = new List<Module> {
-                    new Module { Name = "�豸̨��", Url = "Equipment" },
-                    new Module { Name = "ʵ�鱨��", Url = "Experiment" },
-                    new Module { Name = "Ԥ������", Url = "MaintenanceCycle" },
-                    new Module { Name = "�ල�±�", Url = "Report" },
-                    new Module { Name = "��Ա����", Url = "Certificate" },
-                    new Module { Name = "��������", Url = "Document" }
-            };
-
-            new List<Module> {
-                new Module { Id = 1, Name = "רҵ�ල", Submodules = specialtyModules },
-                new Module { Id = 2, Name = "�ල��̬" },
-                new Module { Id = 3, Name = "�ල��ϵ" },
-                new Module { Id = 4, Name = "�ල����" },
-                new Module { Id = 5, Name = "ϵͳ����", Url= "SystemManagement", Submodules = systemManagementModules }
-            }.ForEach(m => {
-                context.Modules.Add(m);
-            });
-
-            new List<Specialty> {
-                new Specialty { Id = "GHY-DC", Name = "������" },
-                new Specialty { Id = "GHY-DN", Name = "����" },
-                new Specialty { Id = "GHY-HX", Name = "��ѧ" },
-                new Specialty { Id = "GHY-JDBH", Name = "�̵籣��" },
-                new Specialty { Id = "GHY-JN", Name = "����" },
-                new Specialty { Id = "GHY-JS", Name = "����" },
-                new Specialty { Id = "GHY-JY", Name = "��Ե" },
-                new Specialty { Id = "GHY-LC", Name = "����" },
-                new Specialty { Id = "GHY-RG", Name = "�ȹ�" }
-            }.ForEach(s => {
-                s.Modules = specialtyModules;
-                context.Specialties.Add(s);
-            });
-
-            context.SaveChanges();
         }
     }
 }
