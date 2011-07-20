f91aaefe6e4795fe10ac1f0869ff9bc4c9668ae9
Hongfei Xiao <13554344846@163.com>
2011-07-20 20:30:07 +0800

    add jquery.tablseStyle.js

---
+++ WebSite/Scripts/jquery.tableStyle.js
@@ -0,0 +1,19 @@
+﻿if (window.jQuery) {
+    
+    $(document).ready(function () {
+        $("table.datasheet tbody tr").addClass("datasheet_row")
+        .find("th").click(function () {
+            var ptr = $(this).parent("tr");
+            if (ptr.hasClass("selected_row")) {
+                ptr.removeClass("selected_row");
+            }
+            else {
+                $("tr.selected_row").removeClass("selected_row");
+                ptr.addClass("selected_row");
+            }
+            return false;
+        });
+    });
+} else {
+alert("缺少 jquery库文件");
+}
